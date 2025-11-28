dc794f443b99fd72c2f17090d62bfbe6a1bf2dcf
Juan Camilo Pulgarin <juancapulgarin9@gmail.com>
2025-11-28 03:06:54 -0500

    El dto de update listo

--- services/clinica-nestjs/src/clinical-record/dto/update-clinical-record.dto.ts
+++ services/clinica-nestjs/src/clinical-record/dto/update-clinical-record.dto.ts
@@ -1,4 +1,16 @@
+import { IsDateString, IsEnum, IsOptional, IsString } from "class-validator"
+import { cancerStage } from "../entities/clinical-record.entity"
 
 export class UpdateClinicalRecordDto{
+    @IsDateString()
+    @IsOptional()
+    diagnosisDate: string
     
+    @IsEnum(cancerStage)
+    @IsOptional()
+    stage: cancerStage
+    
+    @IsString()
+    @IsOptional()
+    treatmentProtocol: string
 }
